6e6c6f70c536ecb7d8e99077db63f470c9e17c65
Onur Yiğit <45772387+onryiit@users.noreply.github.com>
2024-12-23 21:07:32 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/index.ts',
+  target: 'node',
+  externals: [nodeExternals()], // node_modules dışlanır
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  mode: 'development', // veya production
+};
